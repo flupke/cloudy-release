de1e5885f54cfd54447858cd3f372e0bbed7c4ce
Luper Rouch <luper.rouch@gmail.com>
2013-12-04 14:35:03 +0100

    added a S to /api/deploymentS URLs

--- cloudy/api/urls.py
+++ cloudy/api/urls.py
@@ -5,12 +5,12 @@ from .views import PollDeployment, UpdateNodeStatus, DeploymentCommit
 
 
 urlpatterns = patterns('',
-    url(r'^deployment/(?P<key>[0-9a-f]{32})/poll/$',
+    url(r'^deployments/(?P<key>[0-9a-f]{32})/poll/$',
         csrf_exempt(PollDeployment.as_view()), name='api_poll_deployment'),
-    url(r'^deployment/(?P<key>[0-9a-f]{32})/status/$',
+    url(r'^deployments/(?P<key>[0-9a-f]{32})/status/$',
         csrf_exempt(UpdateNodeStatus.as_view()),
         name='api_update_node_status'),
-    url(r'^deployment/(?P<key>[0-9a-f]{32})/commit/$',
+    url(r'^deployments/(?P<key>[0-9a-f]{32})/commit/$',
         csrf_exempt(DeploymentCommit.as_view()),
         name='api_deployment_commit'),
 )
