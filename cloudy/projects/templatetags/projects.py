613dbfb0c840d7ca65001a426d88233ea8d427fe
Luper Rouch <luper.rouch@gmail.com>
2013-12-12 17:58:38 +0100

    made a special template filter to truncate VCS urls

---
+++ cloudy/projects/templatetags/projects.py
@@ -0,0 +1,17 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def truncate_vcs_url(url, commit_len=6):
+    '''
+    Truncate a VCS *url* so that commit part length does not exceeds
+    *commit_len*.
+    '''
+    prefix, _, commit = url.rpartition('@')
+    if len(commit) > commit_len:
+        commit = commit[:commit_len] + '...'
+    return '%s@%s' % (prefix, commit)
+
