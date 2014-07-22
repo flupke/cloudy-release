84765bb2ab66ca7a84ba9b900a5e844fdfdae04f
Luper Rouch <luper.rouch@gmail.com>
2014-07-22 10:15:02 +0200

    handling None in truncate_vcs_url

--- cloudy/projects/templatetags/projects.py
+++ cloudy/projects/templatetags/projects.py
@@ -10,6 +10,8 @@ def truncate_vcs_url(url, commit_len=6):
     Truncate a VCS *url* so that commit part length does not exceeds
     *commit_len*.
     '''
+    if url is None:
+        return ''
     prefix, _, commit = url.rpartition('@')
     if len(commit) > commit_len:
         commit = commit[:commit_len] + '...'
