70e06ac6d85539b73b5d0de929565d28f772141b
Luper Rouch <luper.rouch@gmail.com>
2014-11-05 16:23:06 +0100

    using gevent-psycopg2 to patch psycopg2

--- cloudy/wsgi.py
+++ cloudy/wsgi.py
@@ -10,14 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloudy.settings")
 
-try:
-    # Make psycopg2 green
-    import gevent.monkey
-    gevent.monkey.patch_all()
-    import psycogreen.gevent
-    psycogreen.gevent.patch_psycopg()
-except ImportError:
-    pass
+import gevent_psycopg2
+gevent_psycopg2.monkey_patch()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
