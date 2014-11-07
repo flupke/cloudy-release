32f3dc740500dfb67d6c1662b2752bae37d17fc8
Luper Rouch <luper.rouch@gmail.com>
2014-11-07 14:03:53 +0100

    added links to user profiles in logs

--- src/cloudy/users/models.py
+++ src/cloudy/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from annoying.fields import AutoOneToOneField
 
@@ -9,3 +10,6 @@ class UserProfile(models.Model):
 
     user = AutoOneToOneField(User, related_name='profile')
     secret = models.CharField(max_length=32, default=uuid_hex, db_index=True)
+
+    def get_absolute_url(self):
+        return reverse('users_profile', kwargs={'pk': self.pk})
